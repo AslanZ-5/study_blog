c80a91bf8a51072e246af4709274026199be6a35
Aslan <asl.zurabov@gmail.com>
2022-01-03 15:58:30 +0300

    dynamic urls

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import home,room
 
+app_name = 'base'
+
 urlpatterns = [
     path('',home, name='home'),
-    path('room/',room, name='room')
+    path('room/<str:pk>/',room, name='room')
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return render(request,'home.html')
+rooms = [
+    {'id':1, "name": 'lets learn python'},
+    {'id':2, "name": 'Design with me'},
+    {'id':3, "name": 'Front end developers'},
+]
 
-def room(request):
-    return render(request,'room.html')
+def home(request):
+    context = {'rooms':rooms}
+    return render(request,'home.html',context=context)
+
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    print('this is pk---',request.GET.get('pk'))
+    return render(request,'room.html',{'room':room})
