3cd441345054f9dc5fa66ef43258629d5cddef08
Aslan <asl.zurabov@gmail.com>
2022-01-03 16:53:21 +0300

    new models Topic and Message

--- base/views.py
+++ base/views.py
@@ -1,20 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {'id':1, "name": 'lets learn python'},
-    {'id':2, "name": 'Design with me'},
-    {'id':3, "name": 'Front end developers'},
-]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,'home.html',context=context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     print('this is pk---',request.GET.get('pk'))
     return render(request,'room.html',{'room':room})
