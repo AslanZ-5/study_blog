d39a4ab268436384deb5f791d3f344c1e68931a3
Aslan <asl.zurabov@gmail.com>
2022-01-03 14:48:27 +0300

    template inheritance

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello World!')
+    return render(request,'home.html')
 
 def room(request):
-    return HttpResponse("Room")
+    return render(request,'room.html')
