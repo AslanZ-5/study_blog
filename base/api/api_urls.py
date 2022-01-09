b286548a4bbf44db1519884777880854ec149c89
Aslan <asl.zurabov@gmail.com>
2022-01-09 13:36:14 +0300

    api get rooms and get room with id

--- base/api/api_urls.py
+++ base/api/api_urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+
 from . import api_views
 
 urlpatterns = [
-    path('',api_views.getRoutes)
-]
+    path('',api_views.getRoutes),
+    path('rooms/',api_views.getRooms),
+    path('rooms/<str:pk>/',api_views.getRoom),
+    ]
---
+++ base/api/serializers.py
@@ -0,0 +1,8 @@
+from django.db import models
+from base.models import Room
+from rest_framework.serializers import ModelSerializer
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
