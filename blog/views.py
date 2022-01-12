aa8b945843047685fdc7298dfb8503e876d50bfd
Aslan <asl.zurabov@gmail.com>
2022-01-12 15:04:46 +0300

    login required and form_valid

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import query
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.urls import reverse
@@ -6,7 +7,8 @@ from django.urls.converters import SlugConverter
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
 from django.contrib.auth.models import User
-
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
 
@@ -23,6 +25,15 @@ def LikePost(request):
 
 class HomeListView(ListView):
     model = Post
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            obj_list = self.model.objects.filter(Q(title__icontains=query)|
+                                                    Q(body__icontains=query)|
+                                                    Q(author__username__icontains=query))
+        else:
+            obj_list = self.model.objects.all()
+        return obj_list
 
 class PostDetailView(DetailView):
     model = Post
@@ -40,9 +51,13 @@ class PostDetailView(DetailView):
 
 
 
-class AddPostView(CreateView):
+class AddPostView(LoginRequiredMixin,CreateView):
     model = Post
-    fields = ['title','author','body']
+    fields = ['title','body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 
@@ -50,7 +65,7 @@ class AddPostView(CreateView):
 class UpdatePostView(UpdateView):
     model = Post
     slug_field = 'title_tag'
-    fields = ['title','author','body']
+    fields = ['title','body']
 
 
 class DeletePostView(DeleteView):
