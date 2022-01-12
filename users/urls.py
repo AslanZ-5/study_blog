ea08a88b0532532a02ab2714b3aeec850f9cff94
Aslan <asl.zurabov@gmail.com>
2022-01-12 20:51:01 +0300

    password reset

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from os import name
+from django.urls import path,reverse_lazy
 from django.contrib.auth import views as auth_views
 from .views  import (
     login_page,
@@ -20,7 +21,15 @@ urlpatterns = [
     path('profile/<str:pk>',userProfile,name='user-profile'),
     path('edit-user/<int:pk>/',editUser, name='edit_user' ),
     path('change-password/',PasswordsChangeView.as_view(), name='change_password'),
-    path('password/success',password_success,name='password_success'),
+    path('password/success/',password_success,name='password_success'),
+    path('password-reset/',auth_views.PasswordResetView.as_view(template_name='password_reset.html',
+                                                                            email_template_name = 'password_reset_confirm.html'),name='password_reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html',),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
+
+
+
+
 
 
 ]
