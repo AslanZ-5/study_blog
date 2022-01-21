b96bf24da4b050a7a1eb12e79d18dc7721d19486
Aslan <asl.zurabov@gmail.com>
2022-01-21 16:37:41 +0300

    registering all models to  admin site

--- base/admin.py
+++ base/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Room, Topic ,Message,Profile
-admin.site.register(Room)
-admin.site.register(Topic)
-admin.site.register(Message)
-admin.site.register(Profile)
+import django.apps
 
 class BaseAdminArea(admin.AdminSite):
     site_header = 'Base Admin Area'
@@ -13,3 +10,14 @@ base_site.register(Room)
 base_site.register(Topic)
 base_site.register(Message)
 base_site.register(Profile)
+
+
+
+models = django.apps.apps.get_models()
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
+
+admin.site.unregister(django.contrib.sessions.models.Session)
