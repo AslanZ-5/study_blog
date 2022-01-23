22f75353672d4bdb461548ff0037db04b17bc1aa
Aslan <asl.zurabov@gmail.com>
2022-01-23 15:40:38 +0300

    adding email filter manually with (SimpleListFilter)

--- base/admin.py
+++ base/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 from .models import Room, Topic ,Message,Profile
 import django.apps
+from django.contrib.admin import SimpleListFilter
 
+
+
+class EmailFilter(SimpleListFilter):
+    title = ('Email Filter',)
+    parameter_name = 'user_email'
+    def lookups(self,request,model_admin):
+        return (
+            ('has_email','has_email'),
+            ('no_email','no_email')
+        )
+    def queryset(self,request,queryset):
+        if not self.value():
+            return queryset
+        if self.value().lower() == 'has_email':
+            return queryset.exclude(user__email='')
+        if self.value().lower() == 'no_email':
+            return queryset.filter(user__email='')
 class Filter(admin.ModelAdmin):
     list_display = ('id','email','user','bio',)
-    list_filter = ('role',)
+    list_filter = ('role',EmailFilter)
 
 
 class BaseAdminArea(admin.AdminSite):
