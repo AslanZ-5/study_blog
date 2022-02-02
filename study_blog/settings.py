78fac274b959e12c3979c7eb59ea000d410c8106
Aslan <asl.zurabov@gmail.com>
2022-02-02 16:24:11 +0300

    changed data base

--- study_blog/settings.py
+++ study_blog/settings.py
@@ -87,16 +87,16 @@ WSGI_APPLICATION = 'study_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'STUDY',
-        'HOST': 'localhost',
-        'PORT': '5433',
-        'USER': 'postgres',
-        'PASSWORD': 'aslan006'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
