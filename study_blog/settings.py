8bd50062b767778b466d24ee5f7259a361883a0b
мансур зурабов <asl.zurabov@gmail.com>
2022-02-03 11:30:28 +0300

    dj_database_url (chenging default data base to heroku postgresql db

--- study_blog/settings.py
+++ study_blog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -96,6 +96,8 @@ DATABASES = {
 }
 
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
